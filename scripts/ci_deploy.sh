72cf23b59d74a03df2fc722a634954a266af133f
elnardu <elnardu2@gmail.com>
2019-11-08 02:42:04 -0500

    Changes

--- scripts/ci_deploy.sh
+++ scripts/ci_deploy.sh
@@ -5,7 +5,7 @@ set -e
 git config --global user.email "elnardu2@gmail.com"
 git config --global user.name "Elnard Utiushev"
 
-git clone https://elnardu:$GITHUB_TOKEN@github.com/elnardu.github.io.git dist
+git clone https://elnardu:$GITHUB_TOKEN@github.com/elnardu/elnardu.github.io.git dist
 
 pushd dist
 git checkout master
